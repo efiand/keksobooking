5c04429f060b96c525cd832f600cc7f06ac7e0da
Andrey Efimov <efiand@ya.ru>
2022-02-24 12:18:51 +0300

    module2-task1

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+// Утилита общего назначения для получения случайного целого из диапазона
+export const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+// Утилита общего назначения для получения случайного числа с заданной точностью из диапапзона
+export const getRandomPositiveFloat = (a, b, digits = 1) => {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
+
+  return parseFloat(result.toFixed(digits));
+};
