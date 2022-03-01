59989d0e0f288ef6d2f3c65895981134b20e2bfa
efiand <efiand@users.noreply.github.com>
2022-03-01 13:43:29 +0000

    deploy: 5c04429f060b96c525cd832f600cc7f06ac7e0da

--- js/main.js
+++ js/main.js
@@ -1,37 +1,17 @@
 // Утилита общего назначения для получения случайного целого из диапазона
-const getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+export const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
 };
 
 // Утилита общего назначения для получения случайного числа с заданной точностью из диапапзона
-const getRandomFloat = (min, max, decimals = 0) => parseFloat((Math.random() * (max - min) + min).toFixed(decimals));
+export const getRandomPositiveFloat = (a, b, digits = 1) => {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = Math.random() * (upper - lower) + lower;
 
-// Проверка, удовлетворяет ли диапазон данных бизнес-требованиям
-const checkRange = (min, max) => {
-  const errors = [];
-
-  if (min < 0) {
-    errors.push('Первый параметр должен быть положительным числом');
-  }
-  if (max < 0) {
-    errors.push('Второй параметр должен быть положительным числом');
-  }
-  if (max <= min) {
-    errors.push('Второй параметр должен быть больше первого');
-  }
-
-  if (errors.length) {
-    throw new Error(errors.join('. '));
-  }
+  return parseFloat(result.toFixed(digits));
 };
-
-// Получение случайного положительного целого с проверкой соответствия диапазона требованиям
-export const getRamdomPositiveInt = (min, max) => checkRange(min, max) || getRandomInt(min, max);
-
-// Получение случайного положительного числа с заданной точностью с проверкой соответствия диапазона требованиям
-export const getRamdomPositiveFloat = (min, max, decimal = 0) => checkRange(min, max) || getRandomFloat(min, max, decimal);
-
-// console.log(getRamdomPositiveInt(3, 78));
-// console.log(getRamdomPositiveFloat(4.3434, 78.3434, 8));
